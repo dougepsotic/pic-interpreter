5ed3fadf75e93495aa4f44cb7a6f1ec1b28b79dc
Doug Sharpe <doug@epsotic.com>
2019-01-07 18:15:09 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
+// Pic Interpreter
+// (c) 2019 Epsotic
+// 
+// A protoype that uploads an image to AWS S3 and displays image recognition labels
+// from the AWS Rekognition service
+// 
+// Built using React and AWS Amplify
+// 
+// TODO
+// Randomize S3 filenames
+// 
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Amplify from 'aws-amplify';
+import { withAuthenticator, S3Image } from 'aws-amplify-react';
+import aws_exports from './aws-exports';
 import './App.css';
+Amplify.configure(aws_exports); 
+
+const MyTheme = {
+  photoPlaceholder: { 'display': 'none' }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Welcome to Pic Interpreter!</h1>
+        <h2>Upload a picture to learn about its attributes</h2>
+        <S3Image theme={MyTheme} path="pictures/" picker />
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App,{includeGreetings: true});
